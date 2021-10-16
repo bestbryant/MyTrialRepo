e6120c96f465f31cd01e1e2b6f2b115e4f706dd8
Sebrian Beselly Putra <sbe002@un-ihe.org>
2021-10-16 18:50:07 +0200

    Third commit with added hyperlinks

--- trial.py
+++ trial.py
@@ -7,3 +7,11 @@ Created on Sat Oct 16 18:33:56 2021
 
 ## This is a project trial
 # Let's see what we can do with this
+
+## Links
+# Link to learn Git
+# https://docs.cloudera.com/machine-learning/1.2/projects/topics/ml-linking-an-existing-project-to-a-git-remote.html
+
+# and
+
+# https://www.toolsqa.com/git/local-repository-remote-repository/
